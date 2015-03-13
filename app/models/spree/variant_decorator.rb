5c48185c4ff3944c79ad8f06298f840f6967fbb7
MIRiyA <miriya.lee@gmail.com>
2015-03-13 19:52:41 +0900

    fix old price

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -5,8 +5,8 @@ Spree::Variant.class_eval do
   def to_hash
     price = self.prices.find_by(currency: Spree::Config[:presentation_currency]).display_price.to_html
     price_usd = self.prices.find_by(currency: "USD").display_price.to_html
-    old_price = self.product.old_price_in_won
-    old_price_usd = self.product.old_price_in_dollar
+    old_price = self.product.display_html_old_price
+    old_price_usd = self.product.display_html_old_price_in_dollar
     {
       :id => self.id,
       :in_stock => self.in_stock?,
